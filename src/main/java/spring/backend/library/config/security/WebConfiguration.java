87f525b0656a26d6f02c1c3b8626df27792e08fa
Pham Hoang <hoang.pv@isofh.com>
2021-04-13 08:48:27 +0700

    sửa lỗi

--- src/main/java/spring/backend/library/config/security/WebConfiguration.java
+++ src/main/java/spring/backend/library/config/security/WebConfiguration.java
@@ -1,9 +1,13 @@
 package spring.backend.library.config.security;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class WebConfiguration extends WebMvcConfigurationSupport {
@@ -16,5 +20,10 @@ public class WebConfiguration extends WebMvcConfigurationSupport {
     registry.addResourceHandler("/uploads/**")
         .addResourceLocations("file:" + (filesPath.startsWith("/") ? "//" : "") + filesPath + "/");
 
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+    argumentResolvers.add( new PageableHandlerMethodArgumentResolver());
   }
 }
