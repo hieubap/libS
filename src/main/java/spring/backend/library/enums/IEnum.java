4a11f1c75bc5b5d7cbd65602013a8cb3c5b26e61
Pham Hoang <hoang.pv@isofh.com>
2021-04-13 11:11:14 +0700

    sửa enum

--- src/main/java/spring/backend/library/enums/IEnum.java
+++ src/main/java/spring/backend/library/enums/IEnum.java
@@ -1,6 +1,11 @@
 package spring.backend.library.enums;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public interface IEnum {
 
   short getValue();
+
+  @JsonValue
+  String getName();
 }
