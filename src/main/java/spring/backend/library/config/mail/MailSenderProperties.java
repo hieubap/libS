d312de945f88d3242326e0b93638d8a42bc53d17
Tuan Do <tuan2112>
2021-05-21 18:53:24 +0700

    add email service

---
+++ src/main/java/spring/backend/library/config/mail/MailSenderProperties.java
@@ -0,0 +1,7 @@
+package spring.backend.library.config.mail;
+
+public class MailSenderProperties {
+    public static String SIPLAB_EMAIL = "sipmedicaleducation@gmail.com";
+    public static String SIPLAB_PASSWORD = "21121999a";
+
+}
